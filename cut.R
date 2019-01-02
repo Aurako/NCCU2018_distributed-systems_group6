5b6cfb89729ee7cbe38299c4281d109cca94b0be
Kuong Chi Kuan <Lazurite@lazurite.local>
2019-01-02 16:30:11 +0800

    split file when >1GB

---
+++ cut.R
@@ -0,0 +1,21 @@
+#用法：commandline : Rscript cut.R 檔案名.csv
+
+args=commandArgs(trailingOnly =TRUE)
+data <- read.csv(args[1])
+datalen=nrow(data)
+print(datalen)
+
+if (datalen>1048000){
+  num=as.integer(datalen/1048000)
+    for (i in c(1:num)){
+      name=paste0("datapart",i,".csv")
+      print(name)
+      finish<-i*1048000
+      print(finish)
+      start<-1+1048000*(i-1)
+      print(start)
+      datapi<-data[start:finish,]
+      print(datapi)
+      write.csv(datapi, file = name)
+    } 
+}
