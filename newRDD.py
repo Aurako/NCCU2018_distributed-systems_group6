386850f4d1626e52dca475efa1859e58b5aff9ba
Kuong Chi Kuan <Lazurite@lazurite.local>
2019-01-05 05:39:23 +0800

    平均值

--- newRDD.py
+++ newRDD.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 import datetime
 import pyspark
+import pyspark.sql.functions as func
 from pyspark.sql.types import IntegerType
 from pyspark.sql.functions import desc
 from pyspark.sql.functions import sum
@@ -14,11 +15,23 @@ if __name__ == '__main__':
 	DT = DT.withColumn('outputs_output_satoshis', DT['outputs_output_satoshis'].cast(IntegerType()))
 	DT.persist()
 
-	num = DT.groupBy("inputs_input_pubkey_base58").count().sort(desc("count")) #count user's transaction number
-	num.show()
+	numin = DT.groupBy("inputs_input_pubkey_base58").count().sort(desc("count")) #count user's transaction number
+	numin.show()
+	numinavg=numin.select(func.avg("count")).collect()
+	print(numinavg)
 	
-	volume = DT.groupBy("inputs_input_pubkey_base58").sum('outputs_output_satoshis').sort(desc("sum(outputs_output_satoshis)"))	
+	numout = DT.groupBy("outputs_output_pubkey_base58").count().sort(desc("count")) #count user's transaction number
+	numout.show()
+	numoutavg=numout.select(func.avg("count")).collect()
+	print(numoutavg)
+
+	volume = DT.groupBy("inputs_input_pubkey_base58").agg(func.max("outputs_output_satoshis"),func.min("outputs_output_satoshis"),func.avg("outputs_output_satoshis"), func.sum("outputs_output_satoshis")).sort(desc("sum(outputs_output_satoshis)"))	#count user's total amount
 	volume.show()
+	volumeavg=DT.select(func.avg("outputs_output_satoshis")).collect()
+	print(volumeavg)
+
+
+
 
 	spark.stop()
 	print('Done!')
