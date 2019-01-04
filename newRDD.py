0f7554da587f924c1ffd71f33da62fc89c2cc34f
Kuong Chi Kuan <Lazurite@lazurite.local>
2019-01-05 03:13:37 +0800

    交易金額排序

--- newRDD.py
+++ newRDD.py
@@ -3,6 +3,7 @@ import datetime
 import pyspark
 from pyspark.sql.types import IntegerType
 from pyspark.sql.functions import desc
+from pyspark.sql.functions import sum
 
 if __name__ == '__main__':
 	spark = pyspark.SparkContext()
@@ -10,20 +11,14 @@ if __name__ == '__main__':
 	CSV = "gs://test1111bucket/demo1G.csv"
 	data = spark.textFile(CSV, use_unicode=False).map(lambda x:x.replace('"', "")).map(lambda x:x.split(","))
 	DT = sqlContext.createDataFrame(data = data.filter(lambda x:x[0]!='DTime'), schema=data.filter(lambda x:x[0]=='DTime').collect()[0])
+	DT = DT.withColumn('outputs_output_satoshis', DT['outputs_output_satoshis'].cast(IntegerType()))
 	DT.persist()
 
-	#DT = DT.filter(DT['DTime'] != "2018-7-31")
-	result = DT.groupBy("inputs_input_pubkey_base58").count().sort(desc("count"))
-	result.show()
-	#DT = DT.withColumn('outputs_output_satoshis', DT['outputs_output_satoshis'].cast(IntegerType()))
-
-	#sumDT = DT.groupBy("inputs_input_pubkey_base58").sum('outputs_output_satoshis').sort("inputs_input_pubkey_base58").withColumnRenamed('sum(outputs_output_satoshis)', 'Sum_satoshis')
-
-	#finalresult = result.join(sumDT ,'DTime', 'inner')
-	#finalresult = finalresult.withColumn("Sum_BTC", finalresult.Sum_satoshis / 100000000)
-
-	#finalresult = finalresult.drop('Sum_satoshis').sort("inputs_input_pubkey_base58")
-	#finalresult.show()
+	num = DT.groupBy("inputs_input_pubkey_base58").count().sort(desc("count")) #count user's transaction number
+	num.show()
+	
+	volume = DT.groupBy("inputs_input_pubkey_base58").sum('outputs_output_satoshis').sort(desc("sum(outputs_output_satoshis)"))	
+	volume.show()
 
 	spark.stop()
 	print('Done!')
